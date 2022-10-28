af69c0093b0f50059f7fab2b9fedca268b58b754
moses <ogbopinamoses@gmail.com>
2022-10-28 07:47:52 +0100

    profile section with profile image and socials added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import profile_picture from "./assets/profile_img.svg";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Only Page</h1>
-    </div>
+    <main>
+      <section className="profile">
+        <img src={profile_picture} alt="user profile" id="profile_img" />
+
+        <div className="socials">
+          <p id="twitter">pena_mo56</p>
+          <p id="slack">pena56</p>
+        </div>
+      </section>
+    </main>
   );
 }
 
